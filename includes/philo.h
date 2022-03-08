9824a2d79796dadfc96f33e50fbca6203386bd3c
Hiroki Ohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-03-08 17:45:46 +0900

    [feat]:add use_mutex.c

--- includes/philo.h
+++ includes/philo.h
@@ -2,6 +2,7 @@
 # define PHILO_H
 
 # include "stdio.h"
+# include "sys/time.h"
 # include "unistd.h"
 # include "stdlib.h"
 # include "stdbool.h"
---
+++ srcs/use_mutex.c
@@ -0,0 +1,44 @@
+#include "stdio.h"
+#include "pthread.h"
+
+typedef struct  s_mut
+{
+	pthread_mutex_t *mutex;
+	int *cnt;
+}	t_mut;
+
+void *f(void *p)
+{
+	t_mut *t;
+	t = p;
+
+	for (int i = 0; i < 10000; ++i)
+	{
+		pthread_mutex_lock(t->mutex);
+		++*t->cnt;
+		pthread_mutex_unlock(t->mutex);
+	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	int cnt = 0;
+	pthread_t thread1;
+	pthread_t thread2;
+
+	pthread_mutex_t mutex;
+	pthread_mutex_init(&mutex, NULL);
+
+	t_mut t;
+	t.mutex = &mutex;
+	t.cnt = &cnt;
+
+	pthread_create(&thread1, NULL, &f, &t);
+	pthread_create(&thread2, NULL, &f, &t);
+
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+	printf("cnt: %d \n", *t.cnt);
+}
