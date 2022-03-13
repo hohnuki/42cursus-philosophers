a3053b9b0bb40c902ff7cc7da8fbab75335d7ad8
大貫　弘貴 <ohnukihiroki8585@yahoo.co.jp>
2022-03-13 17:47:26 +0900

    [fix]:一旦やり直し

--- philo/srcs/error_message.c
+++
@@ -1,7 +0,0 @@
-#include "../includes/philo.h"
-
-void	error_message(t_info *info)
-{
-	write(2, "Error\n", 6);
-	free_element(info);
-}
--- philo/srcs/free.c
+++
@@ -1,9 +0,0 @@
-#include "../includes/philo.h"
-
-void	free_element(t_info *info)
-{
-	if (info->array != NULL)
-		free (info->array);
-	if (info->fork_status != NULL)
-		free (info->fork_status);
-}
