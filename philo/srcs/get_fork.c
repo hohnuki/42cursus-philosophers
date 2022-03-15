e176fba2013d576f9efa489a849cc0fd73857e9c
大貫　弘貴 <ohnukihiroki8585@yahoo.co.jp>
2022-03-15 22:53:29 +0900

    [TODO]:forkがデットロックする

--- philo/srcs/get_fork.c
+++ philo/srcs/get_fork.c
@@ -4,10 +4,11 @@ static void	get_right_fork(t_philo *philo)
 {
 	size_t	fork_index;
 
-	fork_index = philo->philo_number;
+	printf("\x1b[36m[%zu philo_num %zu]\n\033[m", philo->info->time_to_eat, philo->philo_number);
+	fork_index = philo->philo_number - 1;
 	pthread_mutex_lock(philo->info->forks_mutex[fork_index]);
 	philo->has_fork_right = true;
-//	pthread_mutex_unlock(philo->info->forks_mutex[fork_index]);
+	pthread_mutex_unlock(philo->info->forks_mutex[fork_index]);
 }
 
 static void	get_left_fork(t_philo *philo)
@@ -20,11 +21,12 @@ static void	get_left_fork(t_philo *philo)
 		fork_index = philo->philo_number - 1;
 	pthread_mutex_lock(philo->info->forks_mutex[fork_index]);
 	philo->has_fork_left = true;
-//	pthread_mutex_unlock(philo->info->forks_mutex[fork_index]);
+	pthread_mutex_unlock(philo->info->forks_mutex[fork_index]);
 }
 
 void	get_fork(t_philo *philo)
 {
+	printf("\x1b[36m[%zu philo_num %zu]\n\033[m", philo->info->time_to_eat, philo->philo_number);
 	printf("\x1b[36m[%s philo_num %zu]\n\033[m", "get_fork in", philo->philo_number);
 	pthread_mutex_lock(&(philo->info->shared_mutex));
 	printf("\x1b[36m[%s philo_num %zu]\n\033[m", "fork_lock", philo->philo_number);
