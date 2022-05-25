9471760bad9a335d2d54876de0ae1cf15aa0b5ac
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-05-25 13:07:21 +0900

    [refactor]:eat.cの削除

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -1,5 +1,20 @@
 #include "../include/philo.h"
 
+int	philo_eat(t_philo *philo)
+{
+	if (philo->has_fork_right == true && philo->has_fork_left == true)
+	{
+		philo->last_eat_time = get_time_zero_start(philo);
+		if (time_keeper(philo, philo->data->time_to_eat) == 1 \
+			|| print_action(philo, "is eating") == 1)
+			return (1);
+		put_down_fork(philo);
+		philo->eat_count++;
+	}
+	return (0);
+}
+
+
 int	philo_sleep(t_philo *philo)
 {
 	if (print_action(philo, "is sleeping") == 1 \
